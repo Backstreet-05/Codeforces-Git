db3a19e823236a7560107ab8c2025b0dbd37e2df
Anuran <anuranbasu05@gmail.com>
2024-07-04 20:42:09 +0530

    Adding magical sticks question

---
+++ 2. maths/A_Magical_Sticks.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<(n+1)/2<<endl;
+    return;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for (int i=1; i<=t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
