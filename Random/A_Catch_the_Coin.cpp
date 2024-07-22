90b0ba9dd1c8a49796e3ed60f665e86821af15cc
Anuran <anuranbasu05@gmail.com>
2024-07-22 19:46:07 +0530

    Adding catch the coin

---
+++ Random/A_Catch_the_Coin.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int x, y;
+    cin>>x>>y;
+
+    if (y<-1) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+
+    return;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for (int i=1; i<=t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
